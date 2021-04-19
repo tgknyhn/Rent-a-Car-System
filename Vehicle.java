b6090668d751a21dd8793afa7335432ac564da9b
projegrup <projegrup13@gmail.com>
2021-04-19 18:16:10 +0300

    Vehicle interface added.

---
+++ Vehicle.java
@@ -0,0 +1,17 @@
+
+public interface Vehicle {
+	public int getDamage();
+	public int getStock();
+	public int getPrice();
+	public int getColor();
+	public String getModel();
+	public boolean available();
+	
+	public void setDamage(int a);
+	public void setStock(int a);
+	public void setPrice(int a);
+	public void setColor(int a);
+	public void setModel(int a);
+	public void setAvailable();
+	
+}
