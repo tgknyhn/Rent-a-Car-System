005c5338fcc6b6d5afe3927644fde3a783107940
SeymaCanbaz <64903287+SeymaCanbaz@users.noreply.github.com>
2021-04-20 13:49:00 +0300

    Add files via upload
    Added user interface

---
+++ User.java
@@ -0,0 +1,24 @@
+public interface User {
+    String getID();
+    void setID(String ID);
+
+    String getName();
+    void setName(String name);
+
+    String getLastname();
+    void setLastname(String surname);
+
+    String getEmail();
+    void setEmail(String email);
+
+    String getPassword();
+    void setPassword(String password);
+
+/*    int getAge();
+    void setAge(int age);
+
+    String getCity();
+    void setCity(String city);
+*/
+
+}
