dccb8e750c1f43f6fac0e45c5c35472a12381a85
Mertcan Elçi <mertcanelci@outlook.com>
2021-05-06 13:58:08 +0300

    Update AbstractVehicle.java

--- AbstractVehicle.java
+++ AbstractVehicle.java
@@ -1,71 +1,111 @@
 public abstract class AbstractVehicle implements Vehicle {
-    private final String name;
     private final String brand;
-    private final String type;
+    private final String model;
+    private final String color;
     private String description;
 
-    private double cost;
+    private double price;
     private double discountPercent;
+    private double score;
 
     private int damage;
+    private int scoreCount;
 
-    /*Technical variables: max speed, baggage capacity etc.*/
+    private boolean available;
 
-    AbstractVehicle(String name, String brand, String type, String description, double cost) {
-        this.name = name;
+    AbstractVehicle(String brand, String model, String color, String description, double cost) {
         this.brand = brand;
-        this.type = type;
+        this.model = model;
+        this.color = color;
         this.description = description;
-        this.cost = cost;
+        this.price = cost;
         damage = 0;
         discountPercent = 0;
+        available = true;
+        score = 0;
+        scoreCount = 0;
     }
 
     /* Interface methods*/
 
+    @Override
+    public void giveScore(int score) {
+        this.score = ((this.score * scoreCount) + score) / (scoreCount + 1);
+        scoreCount++;
+    }
+
+    @Override
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public void setCost(double cost) {
-        this.cost = cost;
+    @Override
+    public void setPrice(double price) {
+        this.price = price;
     }
 
+    @Override
     public void setDiscountPercent(double discountPercent) {
         this.discountPercent = discountPercent;
     }
 
+    @Override
     public void setDamage(int damage) {
         this.damage = damage;
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public void setAvailable() {
+        available = !available;
     }
 
+    @Override
     public String getBrand() {
         return brand;
     }
 
-    public String getType() {
-        return type;
+    @Override
+    public String getModel() {
+        return model;
     }
 
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public int getDamage() {
+        return damage;
+    }
+
+    @Override
     public String getDescription() {
         return description;
     }
 
-    public double getCost() {
-        return cost;
+    @Override
+    public double getPrice() {
+        return price;
     }
 
+    @Override
     public double getDiscountPercent() {
         return discountPercent;
     }
 
-    public double getDiscountedCost() {
-        return cost * (discountPercent / 100);
+    @Override
+    public double getDiscountedPrice() {
+        return price * (discountPercent / 100);
     }
 
-    /*Getters for technical variables*/
+    @Override
+    public double getScore() {
+        return score;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return available;
+    }
 }
