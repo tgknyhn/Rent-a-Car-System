d2071973f58759d4631370bbe613e16c51ed872e
Anilulasan <32720374+Anilulasan@users.noreply.github.com>
2021-06-25 04:32:53 +0300

    Now holds vehicles.

--- RentalBranch.java
+++ RentalBranch.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.PriorityQueue;
 
@@ -8,12 +12,31 @@ public class RentalBranch extends Branch {
     private String ID;
     private static int count = 0;
     // Vehicles
-    private PriorityQueue<String> priorityVehicles;
+    //private PriorityQueue<String> priorityVehicles;
+    private ArrayList<Vehicle> vehicles;
 
     public RentalBranch(String branchName) {
         super(branchName);
         createID();
         this.branchName = branchName;
+
+        vehicles = new ArrayList<>();
+        try {
+            FileReader fr = new FileReader("vehicles.txt");
+            BufferedReader br = new BufferedReader(fr);
+
+            String line;
+
+            while( (line = br.readLine()) != null) {
+                String[] tokens = line.split("-");
+                Vehicle temp = new Vehicle(tokens[0] , tokens[1] , Integer.parseInt(tokens[2]));
+                vehicles.add(temp);
+            }
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // Getters //
@@ -30,6 +53,8 @@ public class RentalBranch extends Branch {
     public PriorityQueue<Vehicle> getPriorityVehicles() {
         return null;
     }
+
+    public ArrayList<Vehicle> getVehicles() {return vehicles;}
 
     private void createID(){
         if(count > 9999){
